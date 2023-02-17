fcfcc227b39cd14d1a97f0a4201091043c6a3cf6
bayutresna <bayutresna2000@gmail.com>
2023-02-17 09:18:56 +0700

    episode 53

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,20 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-6">
+    {{-- left side --}}
+    <div>
+        <img src="https://i.pravatar.cc/60" width="60" height="60" alt="" class="rounded-xl">
+    </div>
+    {{-- right side --}}
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold"> skadi</h3>
+            <p class="text-xs"> 
+                Posted 
+                <time> a year ago </time> 
+            </p>
+        </header>
+
+        <p>
+            anggap saja komen ygy
+        </p>
+    </div>
+</article>
